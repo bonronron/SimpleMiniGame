78f65969bbde4c8edb87816eff741c05ad5d1205
ec22054 <r.deuskar@se22.qmul.ac.uk>
2023-03-21 15:47:43 +0000

    collision done

--- source/utils/Rectangle.cpp
+++ source/utils/Rectangle.cpp
@@ -8,9 +8,13 @@ bool Rectangle::inside(float x, float y) const
 
 bool Rectangle::intersects(const Rectangle& rect) const
 {
-    return ((rect.inside(topLeft.x, bottomRight.y)) ||
+    return ((inside(rect.topLeft.x, rect.bottomRight.y)) || 
+        (inside(rect.bottomRight.x, rect.bottomRight.y)) || 
+            (inside(rect.bottomRight.x, rect.topLeft.y)) || 
+                (inside(rect.topLeft.x, rect.topLeft.y)) ||
+        (rect.inside(topLeft.x, bottomRight.y)) ||
         (rect.inside(bottomRight.x, bottomRight.y)) ||
-            (rect.inside(bottomRight.x, topLeft.y)) ||
-                (rect.inside(topLeft.x, topLeft.y)));
+        (rect.inside(bottomRight.x, topLeft.y)) ||
+        (rect.inside(topLeft.x, topLeft.y)));
 }
 
